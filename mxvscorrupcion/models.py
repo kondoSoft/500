790988a9b12c878912dc2277569a3854af91054c
Carlos Gonzalez <cyberpolin@gmail.com>
2017-10-24 20:16:23 -0500

    add tynymce and auto slugify save

--- mxvscorrupcion/models.py
+++ mxvscorrupcion/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from tinymce.models import HTMLField
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -33,16 +35,16 @@ class Pregunta(models.Model):
         return 'Pregunta: %s || Respuesta: %s' %(self.text_pregunta, self.respuesta)
 
 
-        
+
 class Cuestionario(models.Model):
     preguntas = models.ManyToManyField(Pregunta)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return 'Pregunta: %s, created: %s' %(self.preguntas, self.created)
 
-        
-    
+
+
 class Empresa(models.Model):
     nombre = models.CharField(max_length=255)
     sector = models.ForeignKey(Sectores)
@@ -60,8 +62,8 @@ class Empresa(models.Model):
 
 class Articulo(models.Model):
     titulo = models.CharField(max_length=255)
-    contenido = models.TextField()
-    slug =models.SlugField(max_length=255)
+    contenido = HTMLField()
+    slug =models.SlugField(max_length=255, unique=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -69,9 +71,17 @@ class Articulo(models.Model):
         verbose_name = "Articulo"
         verbose_name_plural = "Articulos"
 
+    def save(self, *args, **kwargs):
+        #this line below give to the instance slug field a slug name
+        self.slug = slugify(self.titulo)
+        #this line below save every fields of the model instance
+        super(Articulo, self).save(*args, **kwargs)
+
+        return self
+
     def get_absoulte_url(self):
         return reverse('articulos', args=[self.slug])
 
     def __str__(self):
         return self.titulo
-    
+
