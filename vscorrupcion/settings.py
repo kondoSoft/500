3339b2d823587f884ef1b352efab3a0b052589b0
Williams Rincon <wwallass.0310@gmail.com>
2017-11-21 11:01:33 -0600

    added media url

--- vscorrupcion/settings.py
+++ vscorrupcion/settings.py
@@ -147,9 +147,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-MEDIA_ROOT = BASE_DIR
+MEDIA_ROOT = BASE_DIR+'/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
